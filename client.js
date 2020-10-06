3c0439145a827bb3ab707af1b40ff67b9ae7247a
LeRoy Dahl <ldahl2018@gmail.com>
2020-10-06 11:48:13 -0500

    working with casie update

--- client.js
+++ client.js
@@ -1,48 +1,47 @@
-console.log('Here are all the available people:', people);
-console.log(people);
+let randomPerson = '';
+
 $(document).ready(onReady);
 
-let randomNum = randomNumber(1, 7);
-
-function onReady(){
-    displayGame();
-    $('.personBlock').on('click', playGame);
-// event listeners
-    
+function onReady() {
+    //append divs for all in people
+    appendPeople();
+    pickRandomPerson();
+    $('#peopleDisplay').on('click', '.clickPic', clickedPerson)
 }
 
-function displayGame(){
-    let id =0;
-    for (person of people){
-        id++;
-        $('#personDisplay').append(`
-            <button class="personBlock"data-name="${person.name}" data-id="${id}">
-            <img src="https://github.com/${person.githubUsername}.png?size=250" alt="Profile image of Chris">
-            </button>
-        `)
+function clickedPerson() {
+    let clickedPerson = $(this).data('name');
+
+    if (clickedPerson == randomPerson) {
+        alert(`yes! you found ${randomPerson}`);
+        pickRandomPerson();
+    } else {
+        alert('nope! wrong person, try again');
     }
-    //$('#clickOn').text(`Click on:${$(`button[data-id=${randomNum}]`).name}`)
+
 }
+
+
+function appendPeople() {
+    for (let i = 0; i < people.length; i++) {
+        $('#peopleDisplay').append(`
+            <li data-name=${people[i].name} class="clickPic">
+                <img src="https://github.com/${people[i].githubUsername}.png?size=250" 
+                alt="Profile image of ${people[i].name}" 
+                />
+            </li>
+        `);
+    }
+}
+
+function pickRandomPerson() {
+    let randomNum = randomNumber(0, people.length - 1);
+
+    randomPerson = people[randomNum].name;
+    $('#person').text(randomPerson);
+}
+
+
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (1 + max - min) + min);
 }
-function playGame(){
-    
-    let personSelected = $(this).data('id');
-    if (personSelected == randomNum){
-        alert("Yay!!");
-        randomNum = randomNumber(1, 7);
-        playGame();
-    }
-    else if (personSelected != randomNumber){
-        alert("Wrong Person! Try Again");
-    }
-}
-// function displayPersonToChoose(){
-//     for (elem of people){
-//         if (people)
-//     }
-// }
-let vari = $('.personBlock').data("");
-console.log(vari);
-
